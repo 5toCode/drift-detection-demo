6a36bdb3945e463292cbf9f3df4733de01239d66
5toCode <mike70@gmail.com>
2021-12-16 11:32:17 -0500

    add tags with yor, tf apply

--- security-groups.tf
+++ security-groups.tf
@@ -11,4 +11,14 @@ resource "aws_security_group" "drift_detection_demo" {
       "192.168.1.0/24",
     ]
   }
+  tags = {
+    git_commit           = "3350dc56bf92df8a764a4f1b90fec4679f07e47a"
+    git_file             = "security-groups.tf"
+    git_last_modified_at = "2021-11-23 18:30:29"
+    git_last_modified_by = "brad.dausses@gmail.com"
+    git_modifiers        = "brad.dausses"
+    git_org              = "5toCode"
+    git_repo             = "drift-detection-demo"
+    yor_trace            = "3208b95d-1f6f-4014-a712-9499a891d7b4"
+  }
 }
