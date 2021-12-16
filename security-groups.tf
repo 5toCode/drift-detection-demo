5a7e9d3689cf91f61c97ddecd020dc2c1a5e17f7
5toCode <mike70@gmail.com>
2021-12-16 07:54:47 -0500

    remove legacy config

--- security-groups.tf
+++ security-groups.tf
@@ -11,13 +11,6 @@ resource "aws_security_group" "drift_detection_demo" {
     ]
   }
   tags = {
-    git_commit           = "3350dc56bf92df8a764a4f1b90fec4679f07e47a"
-    git_file             = "security-groups.tf"
-    git_last_modified_at = "2021-11-23 18:30:29"
-    git_last_modified_by = "brad.dausses@gmail.com"
-    git_modifiers        = "brad.dausses"
-    git_org              = "bdausses"
-    git_repo             = "drift-detection-demo"
-    yor_trace            = "c17eeeba-c544-407b-b3c3-2aa715ece090"
+
   }
 }
