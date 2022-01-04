0a9b68e75f0912171a7033226519ecffc75a9bbc
5toCode <mike70@gmail.com>
2022-01-04 15:04:18 -0500

    apply tf security group

--- security-groups-2.tf
+++ security-groups-2.tf
@@ -5,7 +5,7 @@ resource "aws_security_group" "drift_demo_2" {
   vpc_id      = "vpc-00e7e47a"
 
   ingress {
-    self = false
+    self      = false
     from_port = 22
     to_port   = 22
     protocol  = "tcp"
@@ -14,6 +14,16 @@ resource "aws_security_group" "drift_demo_2" {
       "10.0.0.0/16",
       "192.168.1.0/24",
     ]
+  }
+  tags = {
+    git_commit           = "8fa8baf1ad6bbdf622088b86dff8d0fb40040406"
+    git_file             = "security-groups-2.tf"
+    git_last_modified_at = "2022-01-03 21:16:52"
+    git_last_modified_by = "mike70@gmail.com"
+    git_modifiers        = "mike70"
+    git_org              = "5toCode"
+    git_repo             = "drift-detection-demo"
+    yor_trace            = "e2e39618-e117-4493-8478-24ee078bf11e"
   }
 }
 
